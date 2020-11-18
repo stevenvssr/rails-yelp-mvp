5e167b1d226900548953001375352ee633906a4d
Steven Visser <steven_v1987@live.nl>
2020-11-18 13:23:16 +0100

    Rails new with frontend and form gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    # Nested!
+    resources :reviews, only: [:new, :create]
+
+    collection do
+      get 'top'
+    end
+
+    member do
+      get 'chef'
+    end
+  end
+  # get 'restaurants/top', to: 'restaurants#top', as: :top_restaurants
+  # get 'restaurants/:id/chef', to: 'restaurants#chef', as: :chef_restaurant
+
+  resources :reviews, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+category = %w[chinese italian japanese french belgian]
+
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name,
+                    address: Faker::Address.city,
+                    phone_number: Faker::PhoneNumber.phone_number,
+                    category: category[rand(0..5)])
+end
